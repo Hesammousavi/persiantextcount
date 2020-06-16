b33e70744144892d665442a51b1202ac22724a09
Mohammad Sajjad Asaadi <mohammadsajjadasaadi@gmail.com>
2020-06-16 13:04:49 +0430

    Update regex
    Update regex to count Persian and English words and even numbers

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let textCountBtn = document.querySelector('#text-count-btn');
 
 textCountBtn.addEventListener('click' , function() {
     contentFooter.classList.remove('active');
-    let newText = text.value.replace(/[a-z()?!\d%؟!،.;:؛*<>"'@,{}=\[\]`]/gi , '');
+    let newText = text.value.replace(/[()?!\d%؟!،.;:؛*<>"'@,{}=\[\]`]/gi , '');
     newText = newText.replace(/[\n]/gi , ' ');
     newText = newText.split(' ').filter(word => {
-        return word.length > 0 && /[آ-ی]/.test(word);
+        return word.length > 0 && /[آ-یa-zA-Z0-9]/.test(word);
     });
     contentFooter.querySelector('#count').innerHTML = newText.length
     contentFooter.classList.add('active');
